a41e127e842d0c7c66cf56838dcbfb444c205cb5
Ganti <28899559+ganti@users.noreply.github.com>
2020-12-29 08:16:02 +0100

    Remove TXS files

--- tiles/nature1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="nature1" tilewidth="32" tileheight="32" tilecount="1496" columns="8">
- <image source="nature1.png" trans="ff00ff" width="256" height="6000"/>
-</tileset>
