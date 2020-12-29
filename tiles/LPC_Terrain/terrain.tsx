f58cfd6f916938b88fb9f78bd2c313340fa45368
Ganti <28899559+ganti@users.noreply.github.com>
2020-12-29 08:03:10 +0100

    Update Textures

---
+++ tiles/LPC_Terrain/terrain.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="terrain.png" trans="ff00ff" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="höhlenweg" tile="46"/>
+ </terraintypes>
+ <tile id="13" terrain="0,0,0,"/>
+ <tile id="14" terrain="0,0,,0"/>
+ <tile id="45" terrain="0,,0,0"/>
+ <tile id="46" terrain=",0,0,0"/>
+ <tile id="76" terrain=",,,0"/>
+ <tile id="77" terrain=",,0,0"/>
+ <tile id="78" terrain=",,0,"/>
+ <tile id="108" terrain=",0,,0"/>
+ <tile id="109" terrain="0,0,0,0"/>
+ <tile id="110" terrain="0,,0,"/>
+ <tile id="140" terrain=",0,,"/>
+ <tile id="141" terrain="0,0,,"/>
+ <tile id="142" terrain="0,,,"/>
+</tileset>
---
+++ tiles/enviromental/environment_tileset_by_reidyaro_d4v6k4z.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="environment_tileset_by_reidyaro_d4v6k4z" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="environment_tileset_by_reidyaro_d4v6k4z.png" trans="ff00ff" width="512" height="512"/>
+</tileset>
