e55aa678d19a71d412ce06196c11f7531406feac
ganti <28899559+ganti@users.noreply.github.com>
2020-12-29 07:16:48 +0000

    Deploying to gh-pages from master @ a41e127e842d0c7c66cf56838dcbfb444c205cb5 🚀

--- tiles/nature2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="nature2" tilewidth="32" tileheight="32" tilecount="2016" columns="32">
- <image source="nature2.png" trans="ff00ff" width="1024" height="2016"/>
-</tileset>
