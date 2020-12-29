6d0c447193fde2bd9251fd9b262795f0eaf706a6
ganti <28899559+ganti@users.noreply.github.com>
2020-12-29 18:06:11 +0000

    Deploying to gh-pages from master @ 61dd313789b133806ffa853dc90b0154c1bb2efd 🚀

---
+++ tiles/foo/ZRPGBeach.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ZRPGBeach" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../workadventure-map-bitwaescherei/tiles/ZRPG Beach/ZRPGBeach.png" width="1024" height="1024"/>
+</tileset>
---
+++ tiles/foo/fantasy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fantasy" tilewidth="32" tileheight="32" tilecount="1024" columns="8">
+ <image source="../../../workadventure-map-bitwaescherei/fantasy.png" width="256" height="4096"/>
+</tileset>
