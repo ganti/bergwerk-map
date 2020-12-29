61dd313789b133806ffa853dc90b0154c1bb2efd
Ganti <28899559+ganti@users.noreply.github.com>
2020-12-29 19:05:31 +0100

    asdf

---
+++ tiles/foo/Graffitti.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Graffitti" tilewidth="32" tileheight="32" tilecount="40" columns="5">
+ <image source="../../../workadventure-map-bitwaescherei/pngs/Graffitti.png" width="160" height="256"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="250"/>
+   <frame tileid="13" duration="250"/>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="33" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="14" duration="250"/>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="34" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="18" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="38" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="250"/>
+   <frame tileid="19" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="39" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ tiles/foo/xmas_deco.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="xmas_deco" tilewidth="32" tileheight="32" tilecount="512" columns="8">
+ <image source="../../../workadventure-map-bitwaescherei/pngs/xmas_deco.png" width="256" height="2048"/>
+ <tile id="41">
+  <animation>
+   <frame tileid="40" duration="250"/>
+   <frame tileid="41" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="49">
+  <animation>
+   <frame tileid="48" duration="250"/>
+   <frame tileid="49" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="67">
+  <animation>
+   <frame tileid="67" duration="400"/>
+   <frame tileid="75" duration="400"/>
+   <frame tileid="83" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="160">
+  <animation>
+   <frame tileid="160" duration="70"/>
+   <frame tileid="192" duration="70"/>
+  </animation>
+ </tile>
+ <tile id="161">
+  <animation>
+   <frame tileid="161" duration="100"/>
+   <frame tileid="193" duration="70"/>
+  </animation>
+ </tile>
+ <tile id="162">
+  <animation>
+   <frame tileid="162" duration="100"/>
+   <frame tileid="194" duration="70"/>
+  </animation>
+ </tile>
+ <tile id="168">
+  <animation>
+   <frame tileid="168" duration="150"/>
+   <frame tileid="200" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="169">
+  <animation>
+   <frame tileid="169" duration="70"/>
+   <frame tileid="201" duration="70"/>
+  </animation>
+ </tile>
+ <tile id="170">
+  <animation>
+   <frame tileid="170" duration="70"/>
+   <frame tileid="202" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="176">
+  <animation>
+   <frame tileid="176" duration="70"/>
+   <frame tileid="208" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="177">
+  <animation>
+   <frame tileid="177" duration="70"/>
+   <frame tileid="209" duration="70"/>
+  </animation>
+ </tile>
+ <tile id="282">
+  <animation>
+   <frame tileid="282" duration="125"/>
+   <frame tileid="291" duration="80"/>
+   <frame tileid="290" duration="110"/>
+   <frame tileid="283" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
